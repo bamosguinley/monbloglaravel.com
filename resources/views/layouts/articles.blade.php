0332661207963f7a3ede06e1fc216a54364b6fe7
Bamos Guinley <guinleyb@gmail.com>
2024-07-22 13:37:53 +0200

    @forelse @empty

--- resources/views/layouts/articles.blade.php
+++ resources/views/layouts/articles.blade.php
@@ -1,19 +1,17 @@
 @extends('layouts.master')
 
 @section('title')
-    Articles
+Articles
 @endsection
 @section('content')
 <h2>Mes articles</h2>
 
-@if ($articles)
-    @foreach ($articles as $article)
+@forelse ($articles as $article)
     <article>
         <h2>{{$article['title']}}</h2>
         <p>{{$article['body']}}</p>
     </article>
-@endforeach
-@else
+@empty
 <p>Oups 😫! Aucun article trouvé</p>
-@endif
+@endforelse
 @endsection
